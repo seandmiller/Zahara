73168697fc39e3d040a744bce364f8217875a241
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2023-10-11 21:28:17 -0700

    created persona

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Zahara
+> Digital Jamaican Based AI that speaks wisdom and is there to help you throught your life.
+
--- Zahara.py
+++ Zahara.py
@@ -1,74 +1,34 @@
-import os, datetime, pyperclip, openai, config
+import openai, config, context
 from PIL import Image
-from pytesseract import pytesseract
 
-path_to_tesseract =  r'C:\Users\x-admiller\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-pytesseract.tesseract_cmd = path_to_tesseract
 
-print(f'-------------------------------------------------------------------------------- \n **Welcome Back {datetime.date.today()} **\n -------------------------------------------------------------------------------- ')
 
-class zoox_tools:
+class ai_body:
     def __init__(self):
         openai.api_key = config.API_KEY
-        f = open('Zahara.txt', 'r')
-        self.behavior = f.read()
         
-        f.close()
+        self.behavior = context.digital_personal
         return
 
-    def scrape_image(self):
-       try:
-        dirname = "/Users/aseanimiller/Desktop/imagereader"
-        
-        files = os.listdir(dirname)
-        print(files)
-        file = "/" +  files[0]
-        image_path = dirname + file
-        img = Image.open(image_path)
-        text = pytesseract.image_to_string(img)
-        print(f' \nThe text detected from your image:\n -------------------------------------------\n{text}\n------------------------------------------ \nit has also been copied to your clipboard\n')
-        pyperclip.copy(text)
-       except IndexError:
-         print(f' IndexError: No Image File detected, remember to save directly to {dirname}')  
-        
-       for file in files:
-            f = os.path.join(dirname, file)
-            os.remove(f)
-       return
-        
+    def prompt_input(self):
+        _input = input('aseanidmiller: ')
+        return _input    
     
-    def chat_wgpt(self,prompt=False, model="gpt-3.5-turbo",repeat=True):
+    def chat_wgpt(self, model="gpt-3.5-turbo"):
         
-        if not prompt: prompt = input('x-admiller>>: ')
+        prompt = self.prompt_input()
         if prompt == 'exit': return 
         response = openai.ChatCompletion.create(
         model=model,
-        messages=[{'role':'system', 'content': self.behavior},
+        messages=[{'role':'system', 'content': self.behavior.context()},
             {'role':'user', 'content': prompt}])
         message = response.choices[0].message.content
         print(f'\n\n Zahara: {message}\n\n')
         
-        if repeat: self.chat_wgpt()
+        self.chat_wgpt()
        
 
-app = zoox_tools()
-program = { 'image': app.scrape_image,
-            '//chatgpt': app.chat_wgpt}
+app = ai_body()
 
-choice = ''
-
-while True:
-    choice = input('\n #What would you like to do Mr Miller?>>: ')
-    if choice == 'exit': break
-    if choice[-1] == '?':
-        app.chat_wgpt(prompt=choice,repeat=False)
-        continue
-        
-    if not program.get(choice):
-        print('I do not recognize that command\n*Here is a list of keys*\n')
-        for key in program.keys():
-             print('--' + key)    
-        continue
-    
-    program[choice]()
+app.chat_wgpt()
    
