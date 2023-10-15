4847c1875b43ca2d57599dbd03817558c5db9cc4
Aseani Miller <aseanimiller@Aseanis-MacBook-Air.local>
2023-10-14 20:55:13 -0700

    complete change to A.I model

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Zahara
-> Digital Jamaican Based AI that speaks wisdom and is there to help you throught your life.
-
+> An A.I therapist that provides words of encouragement and is mindful of the society that you live in as well as the traumas you have that effect you in your day to day life.
--- Zahara.py
+++ Zahara.py
@@ -1,4 +1,4 @@
-import openai, config, context
+import openai, config, context, gtts, playsound
 from PIL import Image
 
 
@@ -8,23 +8,32 @@ class ai_body:
         openai.api_key = config.API_KEY
         
         self.behavior = context.digital_personal
+        self.history = [{'role':'system', 'content': self.behavior.context()},
+            {'role':'system', 'content': self.behavior.society()},
+            {'role':'system', 'content': self.behavior.client()}]
         return
 
     def prompt_input(self):
         _input = input('aseanidmiller: ')
         return _input    
-    
+    def audio_output(self, text):
+        tts = gtts.gTTS(text, lang='en-gh')
+        tts.save('Zahara.mp3')
+        playsound.playsound('Zahara.mp3')
+        return
+
     def chat_wgpt(self, model="gpt-3.5-turbo"):
         
         prompt = self.prompt_input()
         if prompt == 'exit': return 
+        self.history.append({'role':'user', 'content': prompt})
         response = openai.ChatCompletion.create(
         model=model,
-        messages=[{'role':'system', 'content': self.behavior.context()},
-            {'role':'user', 'content': prompt}])
+        messages=self.history)
         message = response.choices[0].message.content
         print(f'\n\n Zahara: {message}\n\n')
-        
+        self.history.append({'role':'assistant','content':message})
+        self.audio_output(message)
         self.chat_wgpt()
        
 
